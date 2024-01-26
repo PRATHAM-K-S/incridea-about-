5bfbc135567502521f4c6e032b8bd99ec6910848
PRATHAM K S <prathamks2302@gmail.com>
2024-01-26 17:53:25 +0530

    updated some files

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,63 @@
+"use client"
 import Image from "next/image";
+import { Poppins } from "next/font/google";
+import localFont from 'next/font/local';
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
+const poppinsLight = Poppins({
+  subsets: ["latin"],
+  weight: "300",
+  style: "normal"
+})
+
+const poppinsBold = Poppins({
+  subsets: ["latin"],
+  weight: "600",
+  style: "normal"
+})
+
+const viking = localFont({
+  src: "./viking_hell/Viking Hell.otf"
+})
+
 
 export default function Home() {
+  useEffect(() => {
+    // Your GSAP animations go here
+    gsap.fromTo("#welcome", { x: "-60vw" }, { x: 0, delay: 0.5, duration: 0.75 })
+    gsap.fromTo("#NMAMIT", { x: "-60vw" }, { x: 0, delay: 1.25, duration: 0.75 })
+    gsap.fromTo("#block", { x: "50vw" }, { x: 0, delay: 0.75, duration: 1 })
+    gsap.fromTo("#nav", { y: "-20vh" }, { y: 0, delay: 0.25, duration: 1 })
+    gsap.fromTo("#campusimg", { y: "30vh", opacity: 0 }, { y: 0, opacity: 1, delay: 1.25, duration: 0.75 })
+    gsap.fromTo("#aboutpara1", { y: "50vh", opacity: 0 }, { y: 0, opacity: 1, delay: 1.50, duration: 0.75 })
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen overflow-x-hidden w-screen bg-gradient-to-br from-[#08011C] to-[#17002D] to-50% px-8 pt-8">
+      <div id="nav" className="flex justify-between items-center mb-16 ">
+        <Image id="hamburger-icon" src={"/Hamburger.svg"} alt="hamburger" width={24} height={24} className="hamburger" />
+        <Image src={"/incredialogo.svg"} alt="hamburger" width={81.33} height={32} />
+        <Image src={"/Avatar.svg"} alt="hamburger" width={32} height={32} />
+      </div>
+      <div className="flex gap-8 items-center">
+        <div>
+          <h1 id="welcome" className={`text-white ${viking.className} text-[32px]`}>Welcome To</h1>
+          <h1 id="NMAMIT" className={`${viking.className} text-[#ffcd38] text-[64px]`}>NMAMIT</h1>
+        </div>
+        <div>
+          <Image id="block" src={"/Blocks.png"} alt="block" width={90} height={90} />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <br />
+      <div className="flex justify-center">
+        <Image id="campusimg" src={"/nittecampus.png"} alt="block" width={450} height={100} />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <br />
+      <br />
+      <div id="aboutpara1">
+        <h4 className={`inline text-[#9F9F9F] font-bold ${poppinsBold.className}`}>Nitte Mahalinga Adyantaya Memorial Institute of Technology</h4>
+        <p className={`text-[#9F9F9F] inline ${poppinsLight.className} `}>(NMAMIT), Nitte, established in 1986 and recognized by the All India Council for Technical Education, New Delhi, has been a constituent college of Nitte (Deemed to be University), Mangaluru,since June 2022. Rank band 101-150 in the National Institutional Ranking Framework (NIRF) 2023 by Ministry of Education, Government of India, the College has been placed under ‘Platinum’ category for having high industry linkages by the AICTE-CII Survey of Industry-Linked Technical Institutes 2020. NMAMIT, the off-campus centre of Nitte DU located at Nitte village, has active collaborations with several international universities and organizations for faculty and student exchanges, research, internships and placements.</p>
       </div>
-    </main>
-  );
+    </div>
+  )
 }
